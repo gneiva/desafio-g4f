bfb9362649ff38ecef63e87b1273a4f2b86c6a2f
Gabriel Neiva <gneiva@users.noreply.github.com>
2024-11-07 09:45:45 -0300

    especificando nomes das colunas no banco

--- Desafio 2/cartoes-ms/src/main/java/io/github/gneiva/desafio_g4f/desafio_2/cartoes_ms/model/Client.java
+++ Desafio 2/cartoes-ms/src/main/java/io/github/gneiva/desafio_g4f/desafio_2/cartoes_ms/model/Client.java
@@ -1,5 +1,6 @@
 package io.github.gneiva.desafio_g4f.desafio_2.cartoes_ms.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,10 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name = "name")
     private String name;
+    
+    @Column(name = "email")
     private String email;
 }
--- Desafio 2/cartoes-ms/src/main/java/io/github/gneiva/desafio_g4f/desafio_2/cartoes_ms/model/CreditProposal.java
+++ Desafio 2/cartoes-ms/src/main/java/io/github/gneiva/desafio_g4f/desafio_2/cartoes_ms/model/CreditProposal.java
@@ -4,6 +4,7 @@ package io.github.gneiva.desafio_g4f.desafio_2.cartoes_ms.model;
 import java.math.BigDecimal;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,8 @@ public class CreditProposal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name ="credit_limit")
     private BigDecimal creditLimit;
 
     @ManyToOne(cascade = CascadeType.ALL)
